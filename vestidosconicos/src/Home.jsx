3e2c98a9565882d653c345911696e830db2cf5c1
Franjuli <Franjuli440@gmail.com>
2025-11-18 09:23:25 -0300

    Create Home.jsx
    To criando home pq não existia

---
+++ vestidosconicos/src/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import IntroText from "../components/IntroText";
+import Categories from "../components/Categories";
+import Quote from "../components/Quote";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <IntroText />
+      <Categories />
+      <Quote />
+    </>
+  );
+}
