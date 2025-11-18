2f39e2ae233d853c2b3282135672e8a19c0669df
Franjuli <Franjuli440@gmail.com>
2025-11-18 09:24:41 -0300

    Update App.jsx
    H maiusculo

--- vestidosconicos/src/App.jsx
+++ vestidosconicos/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Contato from "./pages/Contact";
 
 function App() {
